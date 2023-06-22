aa0702b9ec8e857e6b32f1a050d7c4e62292b135
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-06-22 00:40:00 -0400

    feat: new attribute to base response

--- src/class/BaseResponse.ts
+++ src/class/BaseResponse.ts
@@ -1,9 +1,12 @@
 import { IBaseResponse } from './../interface/IBaseResponse';
 export class BaseResponse implements IBaseResponse {
     message;
+    operationSuccess;
     data;
-    constructor(message: string, data: any) {
+
+    constructor(message: string, data: any, operationSuccess: boolean) {
         this.message = message;
+        this.operationSuccess = operationSuccess;
         this.data = data;
     }
 }
