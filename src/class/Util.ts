41d75067230bcf0d7f7157c4df595bde1a162187
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-07-05 20:00:28 -0400

    feat: send mail function base

--- src/class/Util.ts
+++ src/class/Util.ts
@@ -1,6 +1,7 @@
 import { hash } from 'bcrypt';
 import { v4 } from 'uuid';
 import { BaseResponse } from './BaseResponse';
+import nodemailer, { Transporter } from 'nodemailer';
 class Util {
     public comumFieldValidate(username: string, password: string) {
         if (password === null) {
@@ -21,6 +22,20 @@ class Util {
 
     public async generateUUIDandHash(password: string) {
         return { hashedPassword: await hash(password, 10), uuid: v4() };
+    }
+
+    public async sendResetPasswordBuilder(token: string, email: string) {
+        const transport: Transporter = nodemailer.createTransport({
+            host: 'localhost',
+            port: 1025,
+        });
+        transport.sendMail({
+            from: 'marc.khk11@gmail.com',
+            to: email,
+            subject: 'Envio de Teste',
+            html: `<p>Clique no <a href='http://localhost:8025/new-password/${token}'>Aqui</a> para resetar sua senha</p>`,
+            text: 'chegouuuu',
+        });
     }
 }
 
