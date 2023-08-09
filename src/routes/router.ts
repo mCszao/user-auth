18ff82188235a3ec4e8ae171c91b5e4374e5f9c0
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-09 04:12:04 -0400

    fix: add pattern to routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,9 +4,10 @@ import { BaseResponse } from './../class/BaseResponse';
 import { IUser } from '../interface/IUser';
 import { UserInstance } from '../models/User';
 import MiddlewareCheckValidate from '../class/MiddlewareCheckValidate';
-import { userService } from '../service/UserService';
-import nodemailer from 'nodemailer';
+import UserService from '../service/UserService';
+import UserController from '../controller/UserController';
 import Util from '../class/Util';
+import AddressController from '../controller/AddressController';
 
 const router = Router();
 router.get('/', (req: Request, res: Response) => {
@@ -15,114 +16,18 @@ router.get('/', (req: Request, res: Response) => {
     });
 });
 
-router.use(express.json());
+router.get('/users', UserController.getAll);
+router.get('/users/:username', UserController.getByName);
 
-router.post('/signup', async (req: Request, res: Response) => {
-    try {
-        const record: UserInstance = await userService.add(req.body as IUser);
-
-        return res
-            .status(200)
-            .json(
-                new BaseResponse(
-                    'Cadastro realizado com sucesso!',
-                    record,
-                    true
-                )
-            )
-            .send();
-    } catch (error: any) {
-        res.status(500)
-            .json(
-                new BaseResponse(
-                    'Não foi possível realizar o cadastro!',
-                    error.errors,
-                    false
-                )
-            )
-            .send();
-    }
-});
-
-router.get('/users', async (req: Request, res: Response) => {
-    const limit = req.query?.limit as number | undefined;
-    const offSet = req.query?.offset as number | undefined;
-    try {
-        res.json(
-            new BaseResponse(
-                'Consulta realizada com sucesso',
-                await userService.selectAll(limit, offSet),
-                true
-            )
-        );
-    } catch (error) {
-        res.status(500).json(
-            new BaseResponse('Não foi possível realizar a consulta', {}, false)
-        );
-    } finally {
-        console.log('Transação finalizada');
-    }
-});
-
-router.get('/users/:username', async (req: Request, res: Response) => {
-    try {
-        return res.json(
-            await UserInstance.findOne({
-                where: { username: req.params.username },
-            })
-        );
-    } catch (error) {
-        res.status(500).json(
-            new BaseResponse('Não foi possível realizar a consulta', {}, false)
-        );
-    } finally {
-        console.log('Transação finalizada');
-    }
-});
+router.post('/signup', UserController.signUp);
 
 router.post(
     '/login',
     MiddlewareCheckValidate.loginValidate,
-    async (req: Request, res: Response) => {
-        try {
-            const { username, password }: IUser = req.body;
-            const model: UserInstance | null = await userService.login(
-                username,
-                password
-            );
-            res.status(200).json(
-                new BaseResponse('Login realizado com sucesso!', model, true)
-            );
-        } catch (error: any) {
-            return res
-                .status(500)
-                .json(new BaseResponse('Error', error.message, false))
-                .send();
-        } finally {
-            console.log('Transação finalizada');
-        }
-    }
+    UserController.login
 );
 
-router.post('/user:user_id/address', async (req: Request, res: Response) => {
-    try {
-        const { user_id } = req.params;
-        const address = await AddressInstance.create({ ...req.body, user_id });
-        if (address) {
-            res.status(200)
-                .json(
-                    new BaseResponse(
-                        'Endereço cadastrado com sucesso',
-                        address,
-                        true
-                    )
-                )
-                .send();
-        }
-    } catch (error) {
-        res.status(500).send('Deu ruim');
-    }
-});
+router.post('/user:user_id/address', AddressController.addAddressByUserId);
 
 router.patch('/forgot-password', async (req: Request, res: Response) => {
     let model = null;
@@ -164,7 +69,7 @@ router.patch(
 
 // router.patch('/new-password/:user_id', async (req: Request, res: Response) => {
 //     const { user_id } = req.params;
-    
+
 // });
 
 export default router;
